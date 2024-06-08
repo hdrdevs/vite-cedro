8609a8b33caa71874ec4c2c611ee3569e72ad2c9
Horacio Daniel Ros <horaciodrs@gmail.com>
2024-06-07 21:46:02 -0300

    counter widget

--- src/main.ts
+++ src/main.ts
@@ -1,89 +1,23 @@
-import { Application, WidgetTypes, WidgetAlignTypes } from "cedro/src/";
-import { IWidget } from "cedro/src/interfaces/widget.interface";
-import { Button, Label, Image, Container, Spacer } from "cedro/src/ui";
+import { Application, Widget, WidgetAlignTypes } from "cedro/src/";
 
-class CounterApp extends Application {
-    img: Image;
-    msg: Label;
-    btn: Button;
-    link: Button;
-    counter: number;
-    container: Container;
-
+class MyApplication extends Application {
     constructor() {
-        super("Counter Demo");
+        super("My Application");
         this.getRoot().setAlign(WidgetAlignTypes.HORIZONTAL);
-
-        this.counter = -1;
-
-        this.container = new Container({ orientation: "vertical", size: 300, padding: 8 });
-
-        this.img = new Image("img-id", "cedro-logo.svg");
-        this.img.setType(WidgetTypes.FILL);
-        this.img.setAlternateText("Cedro - Build an UI based on widgets");
-        this.img.setFixedSize(128);
-        this.img.fillHeight();
-
-        this.msg = new Label("label-id", "span");
-        this.msg.setType(WidgetTypes.FILL);
-        this.msg.setFixedSize(80);
-        this.msg.setHCentered(true);
-        this.msg.setVCentered(true);
-
-        this.btn = new Button("btn-id");
-        this.btn.setType(WidgetTypes.FILL);
-        this.btn.setText("Click me");
-        this.btn.setVariant("contained");
-        this.btn.setColor("success");
-        this.btn.setFixedSize(40);
-
-        this.btn.subscribe({
-            event: "click",
-            then: (_w, _e) => {
-                this.counterStep();
-            },
-        });
-
-        this.link = new Button("btn-id");
-        this.link.setType(WidgetTypes.FILL);
-        this.link.setText("Go to Widget Factory");
-        this.link.setVariant("text");
-        this.link.setColor("warning");
-        this.link.setFixedSize(40);
-
-        this.link.subscribe({
-            event: "click",
-            then: (_w, _e) => {
-                this.showLoading();
-                this.router.navigate("/widget-factory");
-            },
-        });
-
-        this.container.addChild(Spacer());
-        this.container.addChild(this.img);
-        this.container.addChild(this.msg);
-        this.container.addChild(this.btn);
-        this.container.addChild(this.link);
-        this.container.addChild(Spacer());
-
-        this.getRoot().addChild(Spacer());
-        this.getRoot().addChild(this.container);
-        this.getRoot().addChild(Spacer());
-
-        this.counterStep();
     }
-
-    public counterStep() {
-        this.counter++;
-        this.msg.setText(`You clicked ${this.counter} times.`);
-    }
-
     init() {
         super.init();
 
+        this.router.on("/", () => {
+            import("./counter/main").then((module) => {
+                this.attachWidget(module.default as Widget, this.getRoot());
+                this.hideLoading();
+            });
+        });
+
         this.router.on("/widget-factory", () => {
             import("./widget-factory/main").then((module) => {
-                this.attachWidget(module.default as IWidget, this.getRoot());
+                this.attachWidget(module.default as Widget, this.getRoot());
                 this.hideLoading();
             });
         });
@@ -95,6 +29,6 @@ class CounterApp extends Application {
     }
 }
 
-export const app = new CounterApp();
+export const app = new MyApplication();
 
 app.init();
